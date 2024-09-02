393101c95286e88c26ff7a34b3035f63a5a85df0
wasayabdul <wasay54@hotmail.com>
2024-09-01 23:56:41 -0400

    Imported modules

---
+++ Chatbot Python Code.py
@@ -0,0 +1,23 @@
+import json
+import openai
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import streamlit as st
+import yfinance as yf
+
+openai.api_key = open('API_KEY', 'r').read()
+
+
+def get_stock_price(ticker):
+    return str(yf.Ticker(ticker).history(period='1y').iloc[-1].Close)
+
+
+def calculate_SMA(Ticker, window):
+    data = yr.Ticker(ticker).history(period='1y').Close
+    return str(data.rolling(window=window).mean().iloc[-1])
+
+
+def calculate_SMA(Ticker, window):
+    data = yr.Ticker(ticker).history(period='1y').Close
+    return str(data.ewm(span=window).mean().iloc[-1])
