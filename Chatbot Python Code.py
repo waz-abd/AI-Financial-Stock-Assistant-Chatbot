e5fc98f3cda3d25d0b19b15b6c262a8937875254
wasayabdul <wasay54@hotmail.com>
2024-09-02 19:15:36 -0400

    Update Chatbot Python Code.py

--- Chatbot Python Code.py
+++ Chatbot Python Code.py
@@ -175,5 +175,45 @@ if user_input:
             {'role': 'user', 'content': f'{user_input}'})
 
         resposne = openai.ChatCompletion.create(
-            model='gpt-4'
+            model='gpt-3.5=turbo-0613',
+            messages=st.session_state['messages'],
+            functions=functions,
+            function_call='auto'
         )
+
+        response_message = response['choices'][0]['message']
+
+        if response_message.get('function_call'):
+            function_name = response_message['function_call']['name']
+            function_args = json.loads(
+                response_message['function_call']['arguments'])
+            if function_name in ['get_stock_price', 'calculate_RSI', 'calculate_MACD', 'plot_stock_price']:
+                args_dict = {'ticker': fucntion_args.get('ticker')}
+            elif function_name in ['calculate_SMA', 'calculate_EMA']:
+                args_dict = {'ticker': function_args.get(
+                    'ticker'), 'window': function_args.get('window')}
+
+        function_to_call = avaliable_functions[function_name]
+        function_response = function_to_call(**args_dict)
+
+        if function_name == 'plot_stock_price':
+            st.image('stock.png')
+        else:
+            st.session_state['messages'].append(response_message)
+            st.session_state['messages'].append(
+                {
+                    'role': 'function',
+                    'name': function_name,
+                    'content': function_response
+                }
+            )
+            second_response = openai.ChatCompletion.create(
+                model='gpt-3.5=turbo-0613',
+                messages=st.session_state['messages']
+            )
+            st.text(second_response['choice'][0]['message']['content'])
+            st.text(second_response['messages'].append(
+                {'role': 'assistant', 'content': response_message['content']}))
+            st.session_state['mes']
+
+    except:
