0b309602c63e0a3128d7edf721de0646e8c100d0
wasayabdul <wasay54@hotmail.com>
2024-09-02 00:05:27 -0400

    Update Chatbot Python Code.py

--- Chatbot Python Code.py
+++ Chatbot Python Code.py
@@ -13,11 +13,21 @@ def get_stock_price(ticker):
     return str(yf.Ticker(ticker).history(period='1y').iloc[-1].Close)
 
 
-def calculate_SMA(Ticker, window):
+def calculate_SMA(ticker, window):
     data = yr.Ticker(ticker).history(period='1y').Close
     return str(data.rolling(window=window).mean().iloc[-1])
 
 
-def calculate_EMA(Ticker, window):
+def calculate_EMA(ticker, window):
+    data = yr.Ticker(ticker).history(period='1y').Close
+    return str(data.ewm(span=window, adjust=False).mean().iloc[-1])
+
+
+def calculate_EMA(ticker, window):
+    data = yr.Ticker(ticker).history(period='1y').Close
+    return str(data.ewm(span=window, adjust=False).mean().iloc[-1])
+
+
+def calculate_EMA(ticker, window):
     data = yr.Ticker(ticker).history(period='1y').Close
     return str(data.ewm(span=window, adjust=False).mean().iloc[-1])
